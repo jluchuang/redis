40f83d0b9731870bbd17c8c513a7fb9aa644cff1
jluchuang <jlu_chuang@163.com>
2016-08-31 17:07:31 +0800

    Add comment for sdsnewlen
    N/A
    
    Signed-off-by: jluchuang <jlu_chuang@163.com>

--- src/sds.c
+++ src/sds.c
@@ -79,12 +79,19 @@ static inline char sdsReqType(size_t string_size) {
  * end of the string. However the string is binary safe and can contain
  * \0 characters in the middle, as the length is stored in the sds header. */
 sds sdsnewlen(const void *init, size_t initlen) {
-    void *sh;
+    // 用于为sds申请内存空间
+    void *sh;        
+
+    // 如果内存申请成功，s则指向真正的字符串存储地址buf[]
     sds s;
+
+    // 根据实际子串的长度判断决定需要使用的sdshdr类型
     char type = sdsReqType(initlen);
     /* Empty strings are usually created in order to append. Use type 8
      * since type 5 is not good at this. */
     if (type == SDS_TYPE_5 && initlen == 0) type = SDS_TYPE_8;
+
+    // 根据所需的sdshdr类型动态获取需要申请的内存字节数
     int hdrlen = sdsHdrSize(type);
     unsigned char *fp; /* flags pointer. */
 
@@ -100,6 +107,9 @@ sds sdsnewlen(const void *init, size_t initlen) {
             break;
         }
         case SDS_TYPE_8: {
+            // 下面在编译时宏定义会替换为:
+            // sdshdr8 *sh = (s - sizeof(sizehdr8)); 
+            // 由于变量作用域不同下面的sh覆盖了switch之外的sh
             SDS_HDR_VAR(8,s);
             sh->len = initlen;
             sh->alloc = initlen;
@@ -128,8 +138,12 @@ sds sdsnewlen(const void *init, size_t initlen) {
             break;
         }
     }
+
+    // 如果需要初始化
     if (initlen && init)
         memcpy(s, init, initlen);
+
+    // 所有sds存储的字串都以'\0'结尾
     s[initlen] = '\0';
     return s;
 }
